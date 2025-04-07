9c5627a7993148fd72b80922a8b8a3ef985f2a68
Manassé KIKAYA <110218792+Mister-Mana@users.noreply.github.com>
2025-04-07 18:49:16 +0100

    Add kelasi-manage/resources/lang/sw/translation.php 1744048155956

---
+++ kelasi-manage/resources/lang/sw/translation.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    'dashboard' => 'Dashibodi',
+    'profile' => 'Wasifu',
+    'settings' => 'Mipangilio',
+    'logout' => 'Ondoka',
+    'language' => 'Lugha',
+    'theme' => 'Mandhari',
+    'dark_mode' => 'Hali ya Giza',
+    'light_mode' => 'Hali ya Mwangaza',
+    'classes' => 'Madarasa',
+    'schools' => 'Shule',
+    'users' => 'Watumiaji',
+    'create' => 'Unda',
+    'edit' => 'Hariri',
+    'delete' => 'Futa',
+    'save' => 'Hifadhi',
+    'cancel' => 'Ghairi',
+];
