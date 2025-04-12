99ebc82eaef0749ebfc8ab6d8c0c6455eae607ff
Manassé KIKAYA <110218792+Mister-Mana@users.noreply.github.com>
2025-04-12 11:44:46 +0100

    Add kelasi-manage-new/bootstrap copy/cache/services.php 1744430871753

---
+++ kelasi-manage-new/bootstrap copy/cache/services.php
@@ -0,0 +1,71 @@
+<?php return array (
+  'providers' => 
+  array (
+    0 => 'Illuminate\\Auth\\AuthServiceProvider',
+    1 => 'Illuminate\\Broadcasting\\BroadcastServiceProvider',
+    2 => 'Illuminate\\Bus\\BusServiceProvider',
+    3 => 'Illuminate\\Cache\\CacheServiceProvider',
+    4 => 'Laravel\\Sail\\SailServiceProvider',
+    5 => 'Laravel\\Sanctum\\SanctumServiceProvider',
+    6 => 'Laravel\\Tinker\\TinkerServiceProvider',
+    7 => 'Carbon\\Laravel\\ServiceProvider',
+    8 => 'Termwind\\Laravel\\TermwindServiceProvider',
+    9 => 'Spatie\\LaravelIgnition\\IgnitionServiceProvider',
+    10 => 'Laravel\\Sanctum\\SanctumServiceProvider',
+    11 => 'Laravel\\Tinker\\TinkerServiceProvider',
+    12 => 'App\\Providers\\AppServiceProvider',
+    13 => 'App\\Providers\\AuthServiceProvider',
+    14 => 'App\\Providers\\EventServiceProvider',
+    15 => 'App\\Providers\\RouteServiceProvider',
+  ),
+  'eager' => 
+  array (
+    0 => 'Illuminate\\Auth\\AuthServiceProvider',
+    1 => 'Laravel\\Sanctum\\SanctumServiceProvider',
+    2 => 'Carbon\\Laravel\\ServiceProvider',
+    3 => 'Termwind\\Laravel\\TermwindServiceProvider',
+    4 => 'Spatie\\LaravelIgnition\\IgnitionServiceProvider',
+    5 => 'Laravel\\Sanctum\\SanctumServiceProvider',
+    6 => 'App\\Providers\\AppServiceProvider',
+    7 => 'App\\Providers\\AuthServiceProvider',
+    8 => 'App\\Providers\\EventServiceProvider',
+    9 => 'App\\Providers\\RouteServiceProvider',
+  ),
+  'deferred' => 
+  array (
+    'Illuminate\\Broadcasting\\BroadcastManager' => 'Illuminate\\Broadcasting\\BroadcastServiceProvider',
+    'Illuminate\\Contracts\\Broadcasting\\Factory' => 'Illuminate\\Broadcasting\\BroadcastServiceProvider',
+    'Illuminate\\Contracts\\Broadcasting\\Broadcaster' => 'Illuminate\\Broadcasting\\BroadcastServiceProvider',
+    'Illuminate\\Bus\\Dispatcher' => 'Illuminate\\Bus\\BusServiceProvider',
+    'Illuminate\\Contracts\\Bus\\Dispatcher' => 'Illuminate\\Bus\\BusServiceProvider',
+    'Illuminate\\Contracts\\Bus\\QueueingDispatcher' => 'Illuminate\\Bus\\BusServiceProvider',
+    'Illuminate\\Bus\\BatchRepository' => 'Illuminate\\Bus\\BusServiceProvider',
+    'Illuminate\\Bus\\DatabaseBatchRepository' => 'Illuminate\\Bus\\BusServiceProvider',
+    'cache' => 'Illuminate\\Cache\\CacheServiceProvider',
+    'cache.store' => 'Illuminate\\Cache\\CacheServiceProvider',
+    'cache.psr6' => 'Illuminate\\Cache\\CacheServiceProvider',
+    'memcached.connector' => 'Illuminate\\Cache\\CacheServiceProvider',
+    'Illuminate\\Cache\\RateLimiter' => 'Illuminate\\Cache\\CacheServiceProvider',
+    'Laravel\\Sail\\Console\\InstallCommand' => 'Laravel\\Sail\\SailServiceProvider',
+    'Laravel\\Sail\\Console\\PublishCommand' => 'Laravel\\Sail\\SailServiceProvider',
+    'command.tinker' => 'Laravel\\Tinker\\TinkerServiceProvider',
+  ),
+  'when' => 
+  array (
+    'Illuminate\\Broadcasting\\BroadcastServiceProvider' => 
+    array (
+    ),
+    'Illuminate\\Bus\\BusServiceProvider' => 
+    array (
+    ),
+    'Illuminate\\Cache\\CacheServiceProvider' => 
+    array (
+    ),
+    'Laravel\\Sail\\SailServiceProvider' => 
+    array (
+    ),
+    'Laravel\\Tinker\\TinkerServiceProvider' => 
+    array (
+    ),
+  ),
+);
