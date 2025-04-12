50b68ac0e0a8c966f1ac1a4a93431aa2ef4158d8
Manassé KIKAYA <110218792+Mister-Mana@users.noreply.github.com>
2025-04-12 11:44:43 +0100

    Add kelasi-manage-new/config copy/app.php 1744430869140

---
+++ kelasi-manage-new/config copy/app.php
@@ -0,0 +1,44 @@
+<?php
+
+return [
+    'name' => 'Kelasi Manage',
+    'env' => env('APP_ENV', 'production'),
+    'debug' => env('APP_DEBUG', false),
+    'url' => env('APP_URL', 'http://localhost'),
+    'asset_url' => env('ASSET_URL'),
+    'timezone' => 'Africa/Abidjan',
+    'locale' => 'fr',
+    'fallback_locale' => 'fr',
+    'faker_locale' => 'fr_FR',
+    'key' => env('APP_KEY'),
+    'cipher' => 'AES-256-CBC',
+
+    'providers' => [
+        // Laravel Framework Service Providers...
+        Illuminate\Auth\AuthServiceProvider::class,
+        Illuminate\Broadcasting\BroadcastServiceProvider::class,
+        Illuminate\Bus\BusServiceProvider::class,
+        Illuminate\Cache\CacheServiceProvider::class,
+        // ... autres providers par défaut
+
+        // Package Service Providers...
+        Laravel\Sanctum\SanctumServiceProvider::class,
+        Laravel\Tinker\TinkerServiceProvider::class,
+
+        // Application Service Providers...
+        App\Providers\AppServiceProvider::class,
+        App\Providers\AuthServiceProvider::class,
+        App\Providers\EventServiceProvider::class,
+        App\Providers\RouteServiceProvider::class,
+    ],
+
+    'aliases' => [
+        'App' => Illuminate\Support\Facades\App::class,
+        'Arr' => Illuminate\Support\Arr::class,
+        // ... autres aliases par défaut
+        'Str' => Illuminate\Support\Str::class,
+        'URL' => Illuminate\Support\Facades\URL::class,
+        'Validator' => Illuminate\Support\Facades\Validator::class,
+        'View' => Illuminate\Support\Facades\View::class,
+    ],
+];
